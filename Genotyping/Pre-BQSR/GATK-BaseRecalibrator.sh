e9e57e255f8e1fdd20176e1fbad1e49c19ac565c
Austin Patton <austin.patton@wsu.edu>
2021-09-21 12:57:10 -0700

    Update GATK-BaseRecalibrator.sh

--- Genotyping/Pre-BQSR/GATK-BaseRecalibrator.sh
+++ Genotyping/Pre-BQSR/GATK-BaseRecalibrator.sh
@@ -5,11 +5,11 @@
 module load java
 
 echo "Generating recalibration model for $1" 
-/global/home/users/austinhpatton/software/gatk-4.1.8.1/gatk --java-options "-Xmx6G" BaseRecalibrator \
-        -R /global/scratch/austinhpatton/cichlids/Oreochromis-Reference/Oreochromis.fna \
-        -I /global/scratch/austinhpatton/cichlids/cameroon/preProcessing/mappedBams/$1_markdup.bam \
-	--known-sites /global/scratch/austinhpatton/cichlids/cameroon/Interim-BQSR/bqsr1/Genotyped/bqsr1-BM-cichlids-filteredSNPS.vcf.gz \
-	--known-sites /global/scratch/austinhpatton/cichlids/cameroon/Interim-BQSR/bqsr1/Genotyped/bqsr1-BM-cichlids_filtered_indels.vcf.gz \
-        -O /global/scratch/austinhpatton/cichlids/cameroon/Interim-BQSR/bqsr1/RecalTables/$1_base-recal.table >& \
-	/global/scratch/austinhpatton/cichlids/cameroon/gatk-output/RecalTables/$1-base-recal-table.out
+/global/scratch/users/austinhpatton/software/gatk-4.1.8.1/gatk --java-options "-Xmx6G" BaseRecalibrator \
+        -R /global/scratch/users/austinhpatton/cichlids/Oreochromis-Reference-2018/GCF_001858045.2_O_niloticus_UMD_NMBU_genomic.fa \
+        -I /global/scratch/users/austinhpatton/cichlids/cameroon/preProcessing/mappedBams_OnilUMD/$1_markdup.bam \
+	--known-sites /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/PreBQSR/HardFilteredVariants/FINAL-BM-cichlids-filteredSNPS.vcf.gz \
+	--known-sites /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/PreBQSR/HardFilteredVariants/FINAL-BM-cichlids_filtered_indels.vcf.gz \
+        -O /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/RecalTables/$1_base-recal.table >& \
+	/global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/RecalTables/$1-base-recal-table.out
 echo "done"
