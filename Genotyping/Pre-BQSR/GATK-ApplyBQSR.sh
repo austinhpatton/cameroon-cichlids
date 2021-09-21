282f8d09e45fe556663f9bd65643c84bedac6dee
Austin Patton <austin.patton@wsu.edu>
2021-09-21 13:01:27 -0700

    Update GATK-ApplyBQSR.sh

--- Genotyping/Pre-BQSR/GATK-ApplyBQSR.sh
+++ Genotyping/Pre-BQSR/GATK-ApplyBQSR.sh
@@ -5,9 +5,9 @@
 module load java
 
 echo "Applying BQSR to $1" 
-/global/home/users/austinhpatton/software/gatk-4.1.8.1/gatk --java-options "-Xmx6G" ApplyBQSR \
-  -R /global/scratch/austinhpatton/cichlids/Oreochromis-Reference/Oreochromis.fna \
-  -I /global/scratch/austinhpatton/cichlids/cameroon/preProcessing/mappedBams/$1_markdup.bam \
-  --bqsr-recal-file /global/scratch/austinhpatton/cichlids/cameroon/Interim-BQSR/bqsr1/RecalTables/$1_base-recal.table \
-  -O /global/scratch/austinhpatton/cichlids/cameroon/Interim-BQSR/bqsr1/BQSR-Bams/$1_BQSR.bam >& /global/scratch/austinhpatton/cichlids/cameroon/gatk-output/ApplyBQSR/$1-apply-BQSR.out
+/global/scratch/users/austinhpatton/software/gatk-4.1.8.1/gatk --java-options "-Xmx6G" ApplyBQSR \
+  -R /global/scratch/users/austinhpatton/cichlids/Oreochromis-Reference-2018/GCF_001858045.2_O_niloticus_UMD_NMBU_genomic.fa \
+  -I /global/scratch/users/austinhpatton/cichlids/cameroon/preProcessing/mappedBams_OnilUMD/$1_markdup.bam \
+  --bqsr-recal-file /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/RecalTables/$1_base-recal.table \
+  -O /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/RecalBams/$1_BQSR.bam >& /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/gatk-output/ApplyBQSR/$1-apply-BQSR.out
 echo "done"
