4928d07f63bc3f839ecb7a380f0b612fbfe6cbc6
Austin Patton <austin.patton@wsu.edu>
2021-09-21 12:13:27 -0700

    Update GATK-Extract-VarsByLongScaff.sh

--- Genotyping/Pre-BQSR/GATK-Extract-VarsByLongScaff.sh
+++ Genotyping/Pre-BQSR/GATK-Extract-VarsByLongScaff.sh
@@ -10,10 +10,10 @@ module load java
 echo "Running scaffold $1 for $2"
 /global/scratch/users/austinhpatton/software/gatk-4.1.8.1/gatk --java-options "-Xmx6G" HaplotypeCaller \
         -R /global/scratch/users/austinhpatton/cichlids/Oreochromis-Reference-2018/GCF_001858045.2_O_niloticus_UMD_NMBU_genomic.fa \
-        -I /global/scratch/users/austinhpatton/cichlids/cameroon/preProcessing/mappedBams_OnilUMD/$2_markdup.bam \
-        --native-pair-hmm-threads $3 \
-        -L $1 \
+        -I /global/scratch/users/austinhpatton/cichlids/cameroon/preProcessing/mappedBams_OnilUMD/$1_markdup.bam \
+        --native-pair-hmm-threads $2 \
+        -L /global/home/users/austinhpatton/cichlids/cameroon/scripts/Onil_UMD/GATK/PreBQSR/ScaffNames.list \
         -ERC GVCF \
-        -O /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/PreBQSR/PerScaff/$2/$2-$1_raw_variants.g.vcf.gz >& \
-        /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/gatk-output/pre-bqsr/$2/$2-$1-call-raw-variants.out
+        -O /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/PreBQSR/PerScaff/$1/$1-ShortScaffs_raw_variants.g.vcf.gz >& \
+        /global/scratch/users/austinhpatton/cichlids/cameroon/Onil_UMD/GATK/gatk-output/pre-bqsr/$1/$1-ShortScaffs-call-raw-variants.out
 echo "done"
